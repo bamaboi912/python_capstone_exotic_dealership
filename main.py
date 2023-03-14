7bb7110542906eb572e401e8567ed4205a8ea8d4
Kejuan Harrison <bamaboi912@icloud.com>
2023-03-14 10:04:05 -0500

    created database

--- main.py
+++ main.py
@@ -14,4 +14,19 @@ def create_server_connection(host_name, user_name, user_password):
         print(f"Error {err}")
     return connection
 
+
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: {err}")
+
+#Queries
+create_database_query = "create database EXOTIC_DEALERSHIP"
+
 connection = create_server_connection("localhost", "root", "student")
+
+create_database(connection, create_database_query)
