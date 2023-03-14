49aaf6fb02ce749f31bb0bc983f47ec53f93eedf
Kejuan Harrison <bamaboi912@icloud.com>
2023-03-14 11:31:03 -0500

    created directory,customer information tables, and populated coupe table

--- main.py
+++ main.py
@@ -34,6 +34,16 @@ def execute_query(connection, query):
     except Error as err:
         print(f"Error: {err}")
 
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print(f"Error: {err}")
+
 
 #Queries
 create_database_query = "create database EXOTIC_DEALERSHIP"
@@ -91,9 +101,45 @@ engine VARCHAR(50) NOT NULL,
 date_of_arrival VARCHAR(50) NOT NULL,
 days_on_the_lot integer NOT NULL);"""
 
+#create directory table
+create_directory_table = """
+create table DIRECTORY_INFORMATON(
+employees_ID VARCHAR(12) PRIMARY KEY,
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL,
+employee_position VARCHAR(50) NOT NULL,
+salary integer NOT NULL,
+phone_number integer NOT NULL,
+years_of_experience integer NOT NULL);"""
+
+#create Customer Information table
+create_customer_information_table = """
+create table CUSTOMER_INFORMATON(
+full_name VARCHAR(12) PRIMARY KEY,
+address VARCHAR(50) NOT NULL,
+email VARCHAR(50) NOT NULL,
+phone_number integer NOT NULL,
+birthday VARCHAR(50) NOT NULL,
+social_security_number integer NOT NULL,
+job_title VARCHAR(50) NOT NULL,
+monthly_income integer NOT NULL);"""
+
+
+#populate coupe table
+coupe_vehicles = """ 
+insert into COUPE_MODELS values
+('123abc321',2017,'Ashton Martin', 'Vanquish', 200, 115000, 'V8','2/18/2023',10),
+('123abc322',2023,'Porsche', '911', 2002, 125000, 'V8','1/18/2023',5),
+('123abc323',2017,'Subaru', 'BRZ', 3000, 28000, 'V6','4/18/2023',30),
+('123abc324',2016,'Dodge', 'Challenger', 20, 30000, 'V8','3/14/2023',15),
+('123abc325',2020,'BMW', 'M4', 100, 74000, 'V8','5/18/2023',1),
+('123abc326',2021,'McLaren', 'Artura', 300, 233000, 'V8','7/19/2023',17),
+('123abc327',2019,'Ford', 'Mustang', 50000, 27000, 'V6','9/8/2023',50),
+('123abc328',2022,'Audi', 'TT', 200, 52000, 'V8','10/1/2023',10)"""
+
 
 
 
 connection = create_server_connection("localhost", "root", "student","exotic_dealership")
 
-execute_query(connection,create_truck_table)
+execute_query(connection,create_customer_information_table)
